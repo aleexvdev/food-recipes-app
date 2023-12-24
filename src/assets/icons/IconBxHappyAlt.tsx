c639cd68e43eb8110bfc433f590f5a4044678930
aleexvdev <valverdealex25@gmail.com>
2023-12-24 18:44:29 -0500

    h1 home page

---
+++ src/assets/icons/IconBxHappyAlt.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+
+function IconBxHappyAlt(props: React.SVGProps<SVGSVGElement>) {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      height="1em"
+      width="1em"
+      {...props}
+    >
+      <path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zm0 18c-4.411 0-8-3.589-8-8s3.589-8 8-8 8 3.589 8 8-3.589 8-8 8z" />
+      <path d="M10 9.5 A1.5 1.5 0 0 1 8.5 11 A1.5 1.5 0 0 1 7 9.5 A1.5 1.5 0 0 1 10 9.5 z" />
+      <path d="M16.986 9.493 A1.493 1.493 0 0 1 15.493 10.986 A1.493 1.493 0 0 1 14 9.493 A1.493 1.493 0 0 1 16.986 9.493 z" />
+      <path d="M12 18c5 0 6-5 6-5H6s1 5 6 5z" />
+    </svg>
+  );
+}
+
+export default IconBxHappyAlt;
--- src/views/ViewHome.tsx
+++ src/views/ViewHome.tsx
@@ -1,3 +1,4 @@
+import IconBxHappyAlt from '../assets/icons/IconBxHappyAlt'
 import { CircleImage } from '../components/common/Foods/CircleImage'
 
 export const ViewHome = () => {
@@ -7,8 +8,20 @@ export const ViewHome = () => {
         <div className="w-[50%] h-full flex flex-col items-center justify-center">
           <div className="w-full h-full flex flex-col items-start justify-center">
             <span className="text-3xl text-orange-500 font-medium mb-3">Recipes</span>
-            <h1 className="text-8xl text-black font-bold tracking-wider mb-6">Good Food</h1>
-            <h1 className="text-8xl text-black font-bold tracking-wider mb-10">Good Mood</h1>
+            <h1 className="text-black font-bold tracking-wider mb-10 flex items-end justify-center">
+              <span className='text-8xl mr-6'>Good</span>
+              <span className='flex items-end justify-center text-8xl'>F<span className='flex items-end justify-center'>
+                <img src={`/assets/images/plato_3.svg`} alt="Foody" title="Foody" className="rounded-full w-full h-auto select-none" />
+                <img src={`/assets/images/plato_5.svg`} alt="Foody" title="Foody" className="rounded-full w-full h-auto select-none" />
+              </span>d</span>
+            </h1>
+            <h1 className="text-black font-bold tracking-wider mb-10 flex items-end justify-center">
+              <span className='text-8xl mr-6'>Good</span>
+              <span className='flex items-end justify-center text-8xl'>M<span className='flex items-end justify-center'>
+                <IconBxHappyAlt className='text-[70px] text-orange-500' />
+                <IconBxHappyAlt className='text-[70px] text-orange-500' />
+              </span>d</span>
+            </h1>
             <p className="font-semibold text-2xl text-gray-900">The food palace is an neighborhood restaurent serving seasonal global cuisine driven by the faire.</p>
           </div>
         </div>
