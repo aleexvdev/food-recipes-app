455976391d5e1a7be6761837c77e3f5da7e1f021
aleexvdev <valverdealex25@gmail.com>
2023-12-24 22:01:23 -0500

    add sticky header

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from 'react';
 import { Header } from '../common/Header/Header'
 
 export const Layout = () => {
+
+  const [isSticky, setIsSticky] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const isScrolled = scrollY > 0;
+
+      setIsSticky(isScrolled);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    // Limpieza del event listener al desmontar el componente
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const headerClass = isSticky ? 'stickyheader' : '';
+
   return (
-    <div className='bg-[#FFFEFC]'>
+    <div className={`bg-[#FFFEFC] w-full ${headerClass}`}>
       <Header />
     </div>
   )
