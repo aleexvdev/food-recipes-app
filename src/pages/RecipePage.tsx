3f8205ddda7b8c7d000807d9951ae81e8283ec36
aleexvdev <valverdealex25@gmail.com>
2023-12-24 21:41:10 -0500

    modified recipes page

--- src/pages/RecipePage.tsx
+++ src/pages/RecipePage.tsx
@@ -5,14 +5,8 @@ export const RecipePage = () => {
         <div className="w-full flex flex-col items-center justify-center">
           <div className="w-full h-full flex flex-col items-center justify-center">
             <span className="text-3xl text-orange-500 font-medium">Recipes</span>
-            <h1 className="text-4xl text-black font-bold">Most Popular Items</h1>
+            <h1 className="text-4xl text-black font-bold">Muy pronto....</h1>
           </div>
-          <div className="w-full h-full flex items-center justify-center mt-10 gap-10">
-            
-          </div>
-          <div className="w-full h-full flex items-center justify-center mt-10">
-            <button className="px-5 py-3 text-white bg-orange-500 rounded-2xl text-xl font-medium hover:bg-orange-600">See More Recipes</button>
-          </div> 
         </div>
       </div>
     </section>
